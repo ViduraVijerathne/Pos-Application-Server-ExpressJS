430d2bdafd0fad91b5aeee1be58df65d48f0d1e0
Vidura Vijerathne <viduranox@gmail.com>
2023-06-22 10:11:02 +0530

    Update auth.js

--- routers/auth/auth.js
+++ routers/auth/auth.js
@@ -23,23 +23,34 @@ router.post('/', async (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
 
-    await database.search(`SELECT * FROM admin WHERE  username = '${username}' AND password = '${password}'`)
-        .then((rows) => {
-            if (rows.length == 1) {
-                const authModel = new AuthModel(rows[0]['username'], rows[0]['password'],generateJwt(req.body), true);
-                const response = JSON.stringify(authModel);
-                console.log("Auth Response : ", response)
-                res.send(response)
-            } else {
-                const authModel = new AuthModel(null, null, null, false);
-                const response = JSON.stringify(authModel);
-                res.send(response)
-            }
-        })
-        .catch((error) => {
-            console.error('Error executing query:', error);
-            res.status(500).send('Internal Server Error');
-        });
+
+    if (username== "vidura" && password == "123") {
+        const authModel = new AuthModel(username, password,generateJwt(req.body), true);
+        const response = JSON.stringify(authModel);
+        console.log("Auth Response : ", response)
+        res.send(response)
+    } else {
+        const authModel = new AuthModel(null, null, null, false);
+        const response = JSON.stringify(authModel);
+        res.send(response)
+    }
+    // await database.search(`SELECT * FROM admin WHERE  username = '${username}' AND password = '${password}'`)
+    //     .then((rows) => {
+    //         if (rows.length == 1) {
+    //             const authModel = new AuthModel(rows[0]['username'], rows[0]['password'],generateJwt(req.body), true);
+    //             const response = JSON.stringify(authModel);
+    //             console.log("Auth Response : ", response)
+    //             res.send(response)
+    //         } else {
+    //             const authModel = new AuthModel(null, null, null, false);
+    //             const response = JSON.stringify(authModel);
+    //             res.send(response)
+    //         }
+    //     })
+    //     .catch((error) => {
+    //         console.error('Error executing query:', error);
+    //         res.status(500).send('Internal Server Error');
+    //     });
 
 
 })
