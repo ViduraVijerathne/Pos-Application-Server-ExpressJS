8815170f88f8c3cdbbb46feefc27408ce2533d12
Vidura Vijerathne <viduranox@gmail.com>
2023-06-29 23:02:20 +0530

    update randome

--- routers/dashboard/dashboard.js
+++ routers/dashboard/dashboard.js
@@ -1,26 +1,32 @@
 const express = require('express');
 const router = express.Router();
 
+// Generate a random integer between min and max (inclusive)
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 router.get('/inhight',(req,res)=>{
     resObj= {body:[
         {
-            title : "Total sales1",
-            count : 25.0,
-        
-        },
-        {
-            title : "Total Sex",
-            count : 251100,
-        
-        },
-        {
-            title : "Total Income 3",
-            count : 6541011,
+            title : "Total Sales",
+            count : getRandomInt(1, 100000),
         
         },
         {
             title : "Total Income",
-            count : 55250.0,
+            count : getRandomInt(1, 100000),
+        
+        },
+        {
+            title : "Total Orders",
+            count : getRandomInt(1, 100000),
+        
+        },
+        {
+            title : "Total Return",
+            count : getRandomInt(1, 100000),
         
         },
        
